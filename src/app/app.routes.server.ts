e7e2f14e6d2d99b61a585450fdd3f5104271bbfa
Ndifabio <nicolasdifabio92@gmial.com>
2026-04-17 01:42:14 -0300

    se agregan configuraciones para el ssr/ssg

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'about', renderMode: RenderMode.Prerender },
+  { path: 'faq', renderMode: RenderMode.Prerender },
+  { path: 'contact', renderMode: RenderMode.Prerender },
+  { path: 'courses', renderMode: RenderMode.Prerender },
+  { path: 'not-found', renderMode: RenderMode.Prerender },
+  { path: 'admin/login', renderMode: RenderMode.Client }, // no necesita SSR/SSG
+  { path: '**', renderMode: RenderMode.Client }, // fallback siempre Client
 ];
